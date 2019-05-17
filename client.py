efebe37370adcd8eb5b6b7a4f0ea11d684a61413
takamatsu-shyo <takamatsu.shyo@gmail.com>
2019-05-17 22:18:48 +0300

    Flask server-client

---
+++ client.py
@@ -0,0 +1,22 @@
+# https://gist.github.com/kylehounslow/767fb72fde2ebdd010a0bf4242371594
+
+import requests
+import json
+import cv2
+
+addr = 'http://localhost:5000'
+test_url = addr + '/api/test'
+
+# Prepare headers for http request
+content_type = 'image/jpeg'
+headers      = {'content-type': content_type}
+
+img = cv2.imread('yaris.jpg')
+
+# Encode image as jpeg
+_, img_encoded = cv2.imencode('.jpg', img)
+# Send HTTP request with image and recieve response
+response = requests.post(test_url, data=img_encoded.tostring(), headers=headers)
+# Decode response
+print (json.loads(response.text))
+
