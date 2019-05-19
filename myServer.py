a655c48cf4f3c5f36b5f6b63cb90e580225aa306
Ubuntu <ubuntu@ip-172-31-45-27.eu-central-1.compute.internal>
2019-05-19 12:57:04 +0000

    Switch to Flask

---
+++ myServer.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, Response
+import jsonpickle
+import numpy as np
+import cv2
+
+# Initilize the Flask application
+app = Flask(__name__)
+
+# Route HTTP posts to this method
+@app.route('/test', methods=['POST'])
+def test():
+    r = request
+    # Convert string of image data to uint8
+    nparr = np.fromstring(r.data, np.uint8)
+    # Decode image
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+    # Do sth?
+
+    # Build a response dict to send back to client
+    response = {'message': 'image received. size={}x{}'.format(img.shape[1], img.shape[0])}
+
+    # Encode response using jsonpicke
+    response_pickled = jsonpickle.encode(response)
+
+    return Response(response=response_pickled, status=200, mimetype="application/json")
+    
+# Start Flask app
+app.run(host="0.0.0.0", port=5000)
