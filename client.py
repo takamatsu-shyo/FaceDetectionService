ab1e5fc8a5bd799fc97c1b49f0203b06471a11db
takamatsu-shyo <takamatsu.shyo@gmail.com>
2019-05-20 22:04:47 +0300

    Dirty elapsed time in client.py

--- client.py
+++ client.py
@@ -3,22 +3,36 @@
 import requests
 import json
 import cv2
+import time
 
 addr = 'http://localhost:5000'
-test_url = addr + '/facebox'
+test_url = addr + '/test'
+facedetect_url = addr + '/facebox'
+
 
 # Prepare headers for http request
 content_type = 'image/jpeg'
 headers      = {'content-type': content_type}
 
+# Read and decode a image
 img = cv2.imread('./photo/jaak_id.jpg')
-
-# Encode image as jpeg
 _, img_encoded = cv2.imencode('.jpg', img)
 
-# Send HTTP request with image and recieve response
-response = requests.post(test_url, data=img_encoded.tostring(), headers=headers)
+start = time.time()
+for i in range(0,11):
+    # Test: Send HTTP request with image and recieve response
+    response = requests.post(test_url, data=img_encoded.tostring(), headers=headers)
+    print (json.loads(response.text))
+elapsed_time = time.time() - start
+print ("elapsed_time:{0}".format(elapsed_time) + "[sec]")
 
-# Decode response
-print (json.loads(response.text))
+
+start = time.time()
+for i in range(0,11):
+    # FaceDetect: Send HTTP request with image and recieve response
+    response = requests.post(facedetect_url, data=img_encoded.tostring(), headers=headers)
+    print (json.loads(response.text))
+elapsed_time = time.time() - start
+print ("elapsed_time:{0}".format(elapsed_time) + "[sec]")
+
 
